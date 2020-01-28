e5fad5296ede2cee83139ed1f38e28672e1784f5
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2020-01-28 05:58:59 -0500

    Added helper functions to grab all links from md files lists

--- test/test_pages.py
+++ test/test_pages.py
@@ -1,18 +1,18 @@
 import requests
 
-def test_index(base='http://192.168.1.5:1313'):
+def test_index(base='http://localhost:1313'):
     """Checks that the home page is available"""
     response = requests.get(base)
     assert response.status_code == 200
 
 
-def test_about_me(base='http://192.168.1.5:1313', path='/pages/about/'):
+def test_about_me(base='http://localhost:1313', path='/pages/about/'):
     """Checks that the about me page is available"""
     response = requests.get(base + path)
     assert response.status_code == 200
 
 
-def test_homelab(base='http://192.168.1.5:1313', path='/pages/homelab/'):
+def test_homelab(base='http://localhost:1313', path='/pages/homelab/'):
     """Checks that the homelab page is available"""
     response = requests.get(base + path)
     assert response.status_code == 200
