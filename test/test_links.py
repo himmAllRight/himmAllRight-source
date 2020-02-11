816560d825bcbc175302842979f4176834e72776
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2020-02-11 06:00:18 -0500

    I had the lower condition backwards

--- test/test_links.py
+++ test/test_links.py
@@ -7,9 +7,9 @@ from constants import BASE_URL
 def test_md_links(post_md_link):
     """Checks that the markdown links are not broken."""
     if post_md_link.startswith("http") or post_md_link.startswith("https"):
-        url = post_md_link.lower()
+        url = post_md_link
     else:
-        url = BASE_URL + post_md_link
+        url = BASE_URL + post_md_link.lower()
 
     response = requests.get(url)
     assert response.status_code == 200, f"The link {post_md_link} is broken."
