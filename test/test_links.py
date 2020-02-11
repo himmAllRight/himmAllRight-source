00ef14f8fc107f279da512f7363fe43c42302abe
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2020-02-11 05:43:52 -0500

    Making all local links lower()

--- test/test_links.py
+++ test/test_links.py
@@ -7,7 +7,7 @@ from constants import BASE_URL
 def test_md_links(post_md_link):
     """Checks that the markdown links are not broken."""
     if post_md_link.startswith("http") or post_md_link.startswith("https"):
-        url = post_md_link
+        url = post_md_link.lower()
     else:
         url = BASE_URL + post_md_link
 
